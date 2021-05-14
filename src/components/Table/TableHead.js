aea3ddcc4cdf663a6622accfffaf8d7bf78dfac4
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-14 23:32:23 +0930

    feat: Implement caret state logic

--- src/components/Table/TableHead.js
+++ src/components/Table/TableHead.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { capitalizeString } from "../../utils/helpers";
 
 function TableHead(props) {
+  // Initialize state object to keep track of caret direction
+  const [caretDown, setCaretDown] = useState(true);
+
+  const renderCaret = (columnAccessor) => {
+    // If the column being sorted by is different to the column invoking this function, don't render caret
+    if (!props.sortColumn || props.sortColumn !== columnAccessor) return;
+
+    return <i className={`fas fa-caret-${caretDown ? "down" : "up"} ms-2`}></i>;
+  };
+
+  const handleClick = (columnAccessor) => {
+    // If user clicks the same column, invert the value of caretDown (i.e Flip caret direction)
+    if (props.sortColumn === columnAccessor)
+      setCaretDown(caretDown ? false : true);
+
+    // If a different column is clicked and caretDown is falsy (i.e. Caret direction is up), change the direction to down (i.e. caretDown = true)
+    if (!caretDown) setCaretDown(true);
+
+    // Invoke handle sort function with the accessor name of the column being clicked
+    props.handleSort(columnAccessor);
+  };
+
   return (
     <thead>
       <tr>
         {props.columns.map((column, index) => (
           <th scope="col" key={index}>
             <button
-              className="border-0 bg-transparent"
-              data-column={column.value}
-              onClick={props.handleSort}
+              className="border-0 bg-transparent text-start px-0"
+              onClick={() => handleClick(column.accessor)}
             >
-              <span className="fw-bold text-secondary">
+              <span
+                className={`fw-bold text-${
+                  props.sortColumn === column.accessor ? "dark" : "muted"
+                }`}
+              >
                 {capitalizeString(column.name)}
               </span>
-
-              <i className="fas fa-caret-down"></i>
+              {renderCaret(column.accessor)}
             </button>
           </th>
         ))}
